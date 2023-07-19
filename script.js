a0a71814e7ac1ecbc1b41ad761f83d0847baf7da
shaswift <shaswift19@gmail.com>
2023-07-19 20:33:40 +0100

    2nd commit

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ function validatePassword() {
     var confirmPassword = document.getElementById("confirmPassword").value;
     var message = document.getElementById("message");
   
-    if (password !== confirmPassword) {
-      message.innerHTML = "Passwords do not match.";
-    } else if (password.length < 10) {
-      message.innerHTML = "Weak password.";
-    } else {
-      message.innerHTML = "Success!";
+    if (password !== ''){ 
+      if (confirmPassword !== ''){
+        if (password !== confirmPassword) {
+          message.innerHTML = "Passwords do not match.";
+          message.style.color = "red"
+        } else if (password.length < 10) {
+          message.innerHTML = "Weak password.";
+          message.style.color = "yellow"
+        } else {
+          message.innerHTML = "Success!";
+          message.style.color = "green"
+        }
+   } else {
+    message.innerHTML = "confirm your password";
+    message.style.color = "yellow"
+   }
+    }
+    else{
+      message.innerHTML = "Fill in your password";
+      message.style.color = "yellow"  
     }
   }
